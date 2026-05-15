43951e47c740c0634ecfd4326d388a23e3ae3cfd
Кристина Мелехина <melehinakristi@gmail.com>
2026-05-15 08:30:56 +0500

    .

--- src/main/java/salon/Appointment.java
+++ src/main/java/salon/Appointment.java
@@ -2,10 +2,8 @@ package salon;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 public class Appointment {
-    private static int nextId = 1;
     private int id;
     private LocalDate date;
     private String time;
@@ -14,9 +12,9 @@ public class Appointment {
     private String services;
     private double cost;
 
-    // Конструктор для создания нового сотрудника
+
+    // Конструктор для создания новой записи
     public Appointment(LocalDate date, String time, String client, String master, String services, double cost) {
-        this.id = nextId++;
         this.date = date;
         this.time = time;
         this.client = client;
@@ -24,7 +22,8 @@ public class Appointment {
         this.services = services;
         this.cost = cost;
     }
-    // Конструктор для редактирования сотрудника (сохраняется старый id)
+
+    // Конструктор для редактирования записи
     public Appointment(int id, LocalDate date, String time, String client, String master, String services, double cost) {
         this.id = id;
         this.date = date;
@@ -35,41 +34,68 @@ public class Appointment {
         this.cost = cost;
     }
 
-    // Геттеры
+    // Геттеры и сеттеры
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public LocalDate getDate() {
         return date;
     }
 
-    public String Time() {
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getTime() {
         return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
     }
 
     public String getClient() {
         return client;
     }
 
+    public void setClient(String client) {
+        this.client = client;
+    }
+
     public String getMaster() {
         return master;
+    }
+
+    public void setMaster(String master) {
+        this.master = master;
     }
 
     public String getServices() {
         return services;
     }
 
+    public void setServices(String services) {
+        this.services = services;
+    }
+
     public double getCost() {
         return cost;
     }
 
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
     // Форматированная дата для отображения в таблице
-    public String getFormattedBirthDate() {
+    public String getFormattedDate() {
         return date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
-    // Переопределяем equals и hashCode для корректного поиска в списке
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
